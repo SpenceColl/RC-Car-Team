6b252256cc2a38da70d4fef8e05f4924840ba012
unknown <student@02511pc.francisparker.org>
2015-03-25 14:42:42 -0700

    Initial commit of MainClass and SteeringWheel.

---
+++ src/MainClass.java
@@ -0,0 +1,65 @@
+import de.hardcode.jxinput.Axis;
+import de.hardcode.jxinput.Button;
+import de.hardcode.jxinput.Directional;
+import de.hardcode.jxinput.JXInputDevice;
+
+
+public class MainClass implements JXInputDevice{
+
+	
+	public static void main(String[] args) {
+
+
+	}
+
+	public Axis getAxis(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Button getButton(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Directional getDirectional(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getMaxNumberOfAxes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMaxNumberOfButtons() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMaxNumberOfDirectionals() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getNumberOfAxes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getNumberOfButtons() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getNumberOfDirectionals() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
